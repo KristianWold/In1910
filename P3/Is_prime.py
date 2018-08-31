ace7872d0eb222dd41083aed5fc24761c4387c6d
KristianWold <kriswold@student.matnat.uio.no>
2018-09-01 00:47:36 +0200

    Started week 3

---
+++ P3/Is_prime.py
@@ -0,0 +1,22 @@
+import sys
+import nose.tools as nt
+def is_prime(n):
+    """Checks if n is prime. n must be an integer"""
+    if not isinstance(n, int):
+        raise ValueError("Argument must be integer")
+
+    if n == 1: return False
+    for i in range(2,n):
+        if n % i == 0:
+            return False
+    return True
+
+def test_is_prime():
+    nt.assert_equal(is_prime(2),True)
+    nt.assert_equal(is_prime(11),True)
+    nt.assert_equal(is_prime(1),False)
+    nt.assert_equal(is_prime(10),False)
+
+@nt.raises(ValueError)
+def test_only_int():
+    is_prime("10")
